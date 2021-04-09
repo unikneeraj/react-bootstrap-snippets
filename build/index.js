b39f213c5c61cd100a943cc32d49cf7cfc893c49
Neeraj Kumar <neerajk2108@gmail.com>
2021-04-09 19:14:41 +0530

    updated

--- build/index.js
+++ build/index.js
@@ -2,44 +2,38 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import fs from 'fs';
 
+const parentDir = path.dirname(path.dirname(fileURLToPath(import.meta.url)));
 
-const parentDirPath = path.dirname(
-  path.dirname(fileURLToPath(import.meta.url))
-);
+const sourceDir = parentDir + '/source';
 
-const sourceDirPath = parentDirPath + '/source';
+const snippetsFile =
+  parentDir + '/react-bootstrap-snippets/snippets/snippets.code-snippets';
 
-const snippetsFile =  parentDirPath + '/react-bootstrap-snippets/snippets/snippets.code-snippets';
-
-
-const files  = [];
+const files = [];
 const snippets = {};
 
-(function searchDir(Directory)  {
-    fs.readdirSync(Directory).forEach(File => {
-        const Absolute = path.join(Directory, File);
-        if (fs.statSync(Absolute).isDirectory()) return searchDir(Absolute);
-        else return files.push(Absolute);
-    });
-})(sourceDirPath)
-
+(function searchDir(Directory) {
+  fs.readdirSync(Directory).forEach(File => {
+    const Absolute = path.join(Directory, File);
+    if (fs.statSync(Absolute).isDirectory()) return searchDir(Absolute);
+    else return files.push(Absolute);
+  });
+})(sourceDir);
 
 files.forEach(file => {
+  const data = fs.readFileSync(file, {
+    encoding: 'utf-8',
+  });
 
-    const data = fs.readFileSync(file, {
-        encoding: 'utf-8'
-    });
-   
-        const fileName = path.basename(file, '.html');
-       
-         snippets[fileName] = {
-             prefix : fileName,
-             body : [data]
-         };
+  const fileName = path.basename(file, '.html');
 
-})
+  snippets[fileName] = {
+    prefix: fileName,
+    body: [data.replace('on-', 'on')],
+  };
+});
 
-fs.writeFile(snippetsFile, JSON.stringify(snippets, null, '\t'), (err)=> {
-    if(err) throw err;
-    console.log('Snippets Generated!');
-})
+fs.writeFile(snippetsFile, JSON.stringify(snippets, null, '\t'), err => {
+  if (err) throw err;
+  console.log('Snippets Generated!');
+});
